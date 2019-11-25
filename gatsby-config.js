cfe92b38bd3bb7e8e040874854d660110566f7a2
Daniel O <danieleugeneoliver@gmail.com>
2019-11-25 17:24:43 -0500

    minor fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Renaissance of Lore`,
+    description: `Renaissance of Lore's website.`,
+    author: `Daniel`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Renaissance of Lore`,
+        short_name: `Renaissance of Lore`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
